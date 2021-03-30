18371be037b32bbeda7187746dde0734ce57cae4
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-03-30 23:04:11 +0200

    Added geocode, and forecast to make weather requests via server.js, provided Bootstrap, and added search form to index.hbs(not working with backend yet).

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Js folder working')
+
+let city = ''
+
+fetch('http://localhost:3000/weather?address=' + city).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error)
+        }
+        console.log(`Location: ` + data.location)
+        console.log(`Forecast: ` + data.forecast)
+    })
+})
